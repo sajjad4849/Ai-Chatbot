28347820b50990d67719d568da75c43124d1793c
sajjad4849 <sajjadahmad3419@gmail.com>
2025-06-07 22:37:45 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4 text-bg-primary">sajjad</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
